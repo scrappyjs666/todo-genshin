59c68458c0bf1aabb540c8705cac15c2fce40429
scrappyjs666 <82704685+scrappyjs666@users.noreply.github.com>
2022-02-09 14:17:25 +0300

    1

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,8 @@
 import '../styles/style.css'
 
 import './includes/test'
+
+//swiper settings
 const slider = document.querySelector('.swiper-container');
 let mySwiper = new Swiper('.swiper-container', {
   spaceBetween: 10,
@@ -22,6 +24,7 @@ let mySwiper = new Swiper('.swiper-container', {
 
 const animation = document.querySelector('.animation');
 const animationtext = document.querySelector('.animation-block__text');
+const animation__image = document.querySelector('.animation__image');
 const form = document.querySelector('.hero__form');
 const formbtn = document.querySelector('.hero-form__button');
 const hero__list = document.querySelector('.hero__list');
@@ -44,7 +47,11 @@ const randomArray = [
   "С тобой приятно иметь дело!",
 ];
 
-animation.addEventListener('click', function() {
+animationtext.addEventListener('click', function() {
+  animationtext.style.color = '#000';
+  animationtext.innerHTML = (randomArray[Math.floor(Math.random() * 4)]);
+});
+animation__image.addEventListener('click', function() {
   animationtext.style.color = '#000';
   animationtext.innerHTML = (randomArray[Math.floor(Math.random() * 4)]);
 });
@@ -55,7 +62,7 @@ let todoList = [];
 function createItem() {
   const todoText = hero__input.value;
   if (todoText.length >= 1 && todoList.length <= 10) {
-    todoList.push(todoText);
+    todoList.unshift(todoText);
     hero__input.value = "";
     localStorage.setItem("todoList", JSON.stringify(todoList));
     renderTodo();
@@ -66,7 +73,7 @@ function createItem() {
   } else if(todoList.length <= 10) {
     animationtext.innerHTML = 'Введи какой-нибудь символ!';
     animationtext.style.color = '#ff0000';
-    animationtext.style.fontSize = '12px';
+    animationtext.style.fontSize = '13px';
   }
   hero__input.focus();
 }
@@ -81,7 +88,9 @@ function renderTodo() {
     itemButton.classList.add("hero-list__btn");
     item.addEventListener("click", (event) => {
       item.classList.toggle('textdecor');
-    }); 
+      todoList.push(item);
+      hero__list.appendChild(item);
+    });
     
     itemButton.addEventListener("click", (event) => {
       todoList.splice(index, 1);
--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,12 @@
-module.exports = {
+module.exports = defineConfig({
   root: 'src',
+  base: '/todo-genshin/dist/',
   build: {
-    outDir: '../dist'
+    outDir: '../dist',
+    rollupOptions: {
+      input: {
+        'index': resolve(__dirname, 'src/index.html'),
+      }
+    }
   }
-}
+})
